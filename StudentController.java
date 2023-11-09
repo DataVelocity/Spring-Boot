888452bb0c2240397f17dafae729953ed7c59c4b
Deepak Raj <67634733+DataVelocity@users.noreply.github.com>
2023-11-09 15:13:11 +0530

    Add files via upload

--- StudentController.java
+++ StudentController.java
@@ -3,11 +3,16 @@ package com.example.demo.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.bean.Student;
+//import org.springframework.http.HttpStatus;
 
 
 @RestController
@@ -21,13 +26,13 @@ public class StudentController {
 //	}
 //	
 	
-	@GetMapping("student")
+	@GetMapping("fun")
 	public Student getStudent() {
 		Student student=new Student(1,"Nayan","Suman");
 		return student;
 	}
  
-	@GetMapping("students")
+	@GetMapping("activitiy")
      public List<Student> funStudent(){
     	 List<Student>students=new ArrayList<>();
     	 students.add(new Student(1, "Arun", "Yadav"));
@@ -42,6 +47,30 @@ public class StudentController {
 			                            @PathVariable("firstName") String firstName,
 			                            @PathVariable("lastName") String lastName     ) {
 		return new Student(studentId,firstName,lastName); 
+	}
+	
+	@PostMapping("activity/create")
+//	@ResponseStatus(HttpStatus.CREATED)
+	public Student createStudent( @RequestBody Student student) {
+		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return student;
+	}
+	
+	@PutMapping("activity/{id}/update")
+	public Student updateStudent( @RequestBody Student student, @PathVariable("id") int studentId) {
+		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return student;
+	}
+	
+	@DeleteMapping("activity/{id}/delete")
+	public String deleteStudent( @PathVariable("id") int studentId) {
+		System.out.println(studentId);
+		
+		return "Student Id Delete Successfully";
 	}
 	
 }
